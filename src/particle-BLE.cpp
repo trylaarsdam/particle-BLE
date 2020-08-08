94deac3080b32838bb9a9f7d3e5176b1af012e6e
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2020-08-08 18:32:34 -0500

    init project creation

---
+++ src/particle-BLE.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "d:/Dev/particle-BLE/src/particle-BLE.ino"
+/*
+ * Project particle-BLE
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "d:/Dev/particle-BLE/src/particle-BLE.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
